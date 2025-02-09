57ba59c64d63f2a22039f620b61f8ead0d15bd18
belarusotvet <151938682+belarusotvet@users.noreply.github.com>
2025-02-09 11:53:10 +0200

    Update createSitemap.js

--- site/createSitemap.js
+++ site/createSitemap.js
@@ -2,6 +2,8 @@ import SitemapGenerator from 'sitemap-generator';
 
 let pagesInSitemap = 0;
 
+
+
 const generator = SitemapGenerator('https://onchainkit.xyz', {
   changeFreq: 'daily',
   ignore: (url) => {
